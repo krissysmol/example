536864ac632c616a99f0c88d50d84fa3101249b3
Krissy! <krissumisu.blueberry.07@gmail.com>
2026-03-19 03:27:47 +0800

    first test

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,21 @@
-#include <Geode/Geode.hpp>
+#include <Geode/Geode.hpp> // The "Everything" file. Gives you access to Geode tools.
+#include <Geode/modify/MenuLayer.hpp> // Tells Geode we want to change the Main Menu.
 
-using namespace geode::prelude;
+using namespace geode::prelude; // Saves you from typing 'geode::' before every command.
+
+// This line tells Geode: "I want to add my own logic to the MenuLayer class."
+class $modify(MenuLayer) {
+    
+    // 'init' is the function that runs when the menu is first created.
+    bool init() {
+        // First, we let the REAL game run its original init code.
+        // If the game fails to load the menu, we stop here.
+        if (!MenuLayer::init()) return false;
+        
+        // This sends a message to the internal console (Logcat/DevTools).
+        log::info("Hello from Acode and GitHub Actions!");
+        
+        // Return true so the game knows the menu loaded successfully.
+        return true;
+    }
+};
