6e67f77221a1b37cd14e24a56696fb57dafe4627
Krissy! <krissumisu.blueberry.07@gmail.com>
2026-03-19 03:43:53 +0800

    update src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#
+
+
 #include <Geode/Geode.hpp> // The "Everything" file. Gives you access to Geode tools.
 #include <Geode/modify/MenuLayer.hpp> // Tells Geode we want to change the Main Menu.
 
@@ -7,15 +10,18 @@ using namespace geode::prelude; // Saves you from typing 'geode::' before every 
 class $modify(MenuLayer) {
     
     // 'init' is the function that runs when the menu is first created.
-    bool init() {
-        // First, we let the REAL game run its original init code.
-        // If the game fails to load the menu, we stop here.
-        if (!MenuLayer::init()) return false;
-        
-        // This sends a message to the internal console (Logcat/DevTools).
-        log::info("Hello from Acode and GitHub Actions!");
-        
-        // Return true so the game knows the menu loaded successfully.
-        return true;
-    }
+bool init() {
+    if (!MenuLayer::init()) return false;
+
+    // This creates a popup box right on your screen!
+    auto alert = FLAlertLayer::create(
+        "Acode Success!",    // Title
+        "My mod is working!", // Message
+        "OK"                 // Button text
+    );
+    alert->show();
+
+    return true;
+}
+
 };
