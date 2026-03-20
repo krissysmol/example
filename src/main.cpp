2f198572a856aa8be6597878c8f1a2d2626aace5
Krissy! <krissumisu.blueberry.07@gmail.com>
2026-03-20 14:38:32 +0800

    update src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,27 @@
-#
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PauseLayer.hpp>
+#include "ui/macro/MainPanel.hpp" // <--- IMPORTANT: Include your new header!
 
+using namespace geode::prelude;
 
-#include <Geode/Geode.hpp> // The "Everything" file. Gives you access to Geode tools.
-#include <Geode/modify/MenuLayer.hpp> // Tells Geode we want to change the Main Menu.
+class $modify(MyPauseLayer, PauseLayer) {
+    void onMacroBtn(CCObject* sender) {
+        // Now just call the custom class
+        MainPanel::create()->show();
+    }
 
-using namespace geode::prelude; // Saves you from typing 'geode::' before every command.
+    bool init() {
+        if (!PauseLayer::init()) return false;
 
-// This line tells Geode: "I want to add my own logic to the MenuLayer class."
-class $modify(MyMenuClass, MenuLayer) {
-    
-    // 'init' is the function that runs when the menu is first created.
-bool init() {
-    if (!MenuLayer::init()) return false;
-
-    // We use a "lambda" (a tiny function) to run the alert later
-    this->scheduleOnce(schedule_selector(MyMenuClass::showMyAlert), 0.5f);
-
-    return true;
-}
-
-// We need to define this function inside your $modify(MenuLayer) class
-void showMyAlert(float dt) {
-    auto alert = FLAlertLayer::create(
-        "Acode Success!", 
-        "My mod is working and staying open!", 
-        "OK"
-    );
-    alert->show();
-}
-
-    void onMoreGames(CCObject* target) {
-        FLAlertLayer::create(
-            "Geode",
-            "Hello World from my Custom Mod!",
-            "OK"
-        )->show(); 
-    };
+        auto menu = this->getChildByID("left-button-menu");
+        if (menu) {
+            auto btnSprite = CCSprite::createWithSpriteFrameName("GJ_plainBtn_001.png");
+            auto btn = CCMenuItemSpriteExtra::create(
+                btnSprite, this, menu_selector(MyPauseLayer::onMacroBtn)
+            );
+            menu->addChild(btn);
+            menu->updateLayout();
+        }
+        return true;
+    }
 };
