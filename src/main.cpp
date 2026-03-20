655ecd65aff45e72e964b1b6df4d88a4b5bebc83
Krissy! <krissumisu.blueberry.07@gmail.com>
2026-03-20 15:42:54 +0800

    hmm

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,22 @@
 using namespace geode::prelude;
 
 class $modify(MyMenuLayer, MenuLayer) {
+	void onMenuLoaded(float dt) {
+		// Show a one-time welcome message
+		auto alert = FLAlertLayer::create(
+			"Mod Loaded",
+			"Your clean template is running!",
+			"OK"
+		); //this is just a variable, nothing to be scared about...
+		alert->show(); // the remote with modifier of the variable, show means showing it basically.
+	};
 	//init function, if called return
-    bool init() {
-        if (!MenuLayer::init()) return false; // Not initialized
-
-        // Verify the mod is working in the console
-        log::info("Example Mod: The mod is working."); //send into the log files
-
-        // Show a one-time welcome message
-        auto alert = FLAlertLayer::create(
-            "Mod Loaded",
-            "Your clean template is running!",
-            "OK"
-        ); //this is just a variable, nothing to be scared about...
-        alert->show(); // the remote with modifier of the variable, show means showing it basically.
-
-        return true; // Initialized
-    }
+	bool init() {
+		if (!MenuLayer::init()) return false; // Not initialized
+		// Verify the mod is working in the console
+		log::info("Example Mod: The mod is working."); //send into the log files
+		this->scheduleOnce(schedule_selector(MyMenuLayer::onMenuLoaded), 0.1f); //always that schedule_selector, then what class and function to call, and time(f for floating point as 0.1 seconds). function from cocos class
+		
+		return true; // Initialized
+	}
 };
