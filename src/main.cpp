bfeb0297b981db435f1b5f693c2a28b833d703bc
Krissy! <krissumisu.blueberry.07@gmail.com>
2026-03-20 16:29:59 +0800

    update src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,15 @@ class $modify(MyMenuLayer, MenuLayer) {
 		); //this is just a variable, nothing to be scared about...
 		alert->show(); // the remote with modifier of the variable, show means showing it basically.
 	};
+	void onEnter() {
+		MenuLayer::onEnter();
+		this->scheduleOnce(schedule_selector(MyMenuLayer::onMenuLoaded), 0.1f); //always that schedule_selector, then what class and function to call, and time(f for floating point as 0.1 seconds). function from cocos class
+	};
 	//init function, if called return
 	bool init() {
 		if (!MenuLayer::init()) return false; // Not initialized
 		// Verify the mod is working in the console
 		log::info("Example Mod: The mod is working."); //send into the log files
-		this->scheduleOnce(schedule_selector(MyMenuLayer::onMenuLoaded), 0.1f); //always that schedule_selector, then what class and function to call, and time(f for floating point as 0.1 seconds). function from cocos class
 		
 		return true; // Initialized
 	}
